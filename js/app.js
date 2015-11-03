b16d790bf02c61736d76f0ca9cf92c13b3086041
Ed Glazenski <edglazenski@gmail.com>
2015-11-03 17:13:32 -0500

    Added Google Analytics

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $('#down-arrow').on("click", function() {
     $(this).velocity("scroll", { duration: 1500, offset: 180, easing: "easeInSine" })
-    
   });
+
+  $('#down-arrow').on('hover', function(){
+    $(this).velocity({
+        translateY: "10px"
+      }, {
+        loop: true
+      }).velocity("reverse");
+    });
 });
